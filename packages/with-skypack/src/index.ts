860eb2f35d4cd674060988d3796d4bb2e6bd9425
Damien Foulhoux <dfoulhoux@openstudio.fr>
2022-04-27 16:22:15 +0200

    add demo skypack module

---
+++ packages/with-skypack/src/index.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import axios from "axios";
+import lodash from "lodash";
+import { nanoid } from "nanoid";
+
+export default () => {
+  console.log(axios, React, nanoid(), lodash);
+};
