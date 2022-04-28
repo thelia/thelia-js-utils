e6840dbd279e744db96081a05229acd84a33c9c6
Mathis Chambon <matchambon60@gmail.com>
2022-04-28 17:02:28 +0200

    Finished Product block

--- packages/blocks-editor/src/blocks/Product/Product.tsx
+++ packages/blocks-editor/src/blocks/Product/Product.tsx
@@ -2,6 +2,15 @@ import { BlockModuleComponentProps, BlockPluginDefinition } from "../../types/ty
 
 import * as React from "react";
 import useSWR from "swr";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowDown,
+  faArrowRight,
+  faArrowUp,
+  faCircleNotch,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { reorder } from "../../utils/array";
 
 export type BlockProductData = {
   productList: string[];
@@ -9,104 +18,185 @@ export type BlockProductData = {
 
 export type BlockProductComponentProps = BlockModuleComponentProps<BlockProductData>;
 
-function Product({
-  product,
+const Product = ({
+  productId,
+  productIndex,
   data,
   onUpdate,
 }: {
-  product: any;
+  productId: any;
+  productIndex: number;
   data: any;
   onUpdate: Function;
-}) {
-  function removeFromList(productId: any) {
-    //Fonction du bouton Supprimer
-    let newProductList = data.productList.filter(function (product: any) {
-      return product != productId;
-    });
-    onUpdate({ ...data, productList: newProductList });
-  }
+}) => {
+  const { data: product } = useSWR(`/product/search?id=${productId}`);
+
   return (
-    <div className="flex flex-row col-span-1 m-4 font-sans bg-gray-100 shadow-lg rounded-2xl">
-      <div className="w-full">
-        <div className="flex flex-col px-8 pb-2 bg-lime-100 rounded-2xl">
-          {product.images.length > 0 ? (
-            <img className="w-full" src="" alt="image" />
-          ) : null}
-          <div className="pt-6 m-2 text-2xl font-bold">{product.i18n.title}</div>
+    <div className="w-full flex justify-between bg-white rounded-md gap-8 p-2 my-4 items-center h-20">
+      {product?.[0]?.images.length > 0 ? (
+        <img className="h-full" src={product?.[0]?.images[0].url} alt="product image" />
+      ) : (
+        <img
+          className="h-full"
+          src="https://via.placeholder.com/150"
+          alt="product image"
+        />
+      )}
+      <div className="font-semibold w-1/5 text-left">{product?.[0]?.i18n.title}</div>
+      <div className="w-1/5 text-left text-gray-400"># {product?.[0]?.reference}</div>
+      <div className="border-x border-gray-400 flex gap-8 px-4">
+        <button
+          className={`${productIndex === 0 && "text-gray-400"}`}
+          disabled={productIndex === 0}
+          onClick={() =>
+            onUpdate({
+              ...data,
+              productList: [...reorder(data.productList, productIndex, productIndex - 1)],
+            })
+          }
+        >
+          <FontAwesomeIcon icon={faArrowUp} />
+        </button>
 
-          <div className="self-end">
-            <span className="m-2 font-sans text-xl text-gray-600 place-self-end">
-              {product.productSaleElements[0].price.untaxed} $
-            </span>
-            <span className="m-2 text-xl font-bold text-gray-700 place-self-end">
-              {product.productSaleElements[0].price.taxed} $
-            </span>
-          </div>
-
-          <div className="py-4">
-            {product.categories.map(function (category: any) {
-              return (
-                <span className="relative inline-flex px-3 py-1 font-sans text-xl font-semibold text-gray-700 bg-gray-200 rounded-full">
-                  <p className="inline-block m-2">{category.i18n.title}</p>
-                </span>
-              );
-            })}
-          </div>
-        </div>
+        <button
+          className={`${productIndex === data.productList.length - 1 && "text-gray-400"}`}
+          disabled={productIndex === data.productList.length - 1}
+          onClick={() =>
+            onUpdate({
+              ...data,
+              productList: [...reorder(data.productList, productIndex, productIndex + 1)],
+            })
+          }
+        >
+          <FontAwesomeIcon icon={faArrowDown} />
+        </button>
       </div>
       <a
-        className="relative inline-block w-12 h-12 m-4 bg-red-400 rounded-full hover:text-black hover:bg-red-600"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={product?.[0]?.url}
+        className="border-2 border-red-500 text-red-500 hover:bg-red-500 hover:text-white inline-block px-4 py-1 rounded-md text-center"
+      >
+        <span className="mr-4">Voir la fiche produit</span>
+        <FontAwesomeIcon icon={faArrowRight} />
+      </a>
+      <button
+        className="self-start hover:text-red-500"
         onClick={() =>
           onUpdate({
             ...data,
-            productList: data.productList.filter((id: any) => id != product.id),
+            productList: data.productList.filter((id: any) => id != product?.[0]?.id),
           })
         }
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="relative w-8 h-8 m-2"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-        </svg>
-      </a>
+        <FontAwesomeIcon icon={faXmark} />
+      </button>
     </div>
   );
-}
+};
 
 function BlockProductComponent({ data, onUpdate }: BlockProductComponentProps) {
-  const [query, setQuery] = React.useState<string>(""); //Recherche par titres
+  const [searchByRef, setSearchByRef] = React.useState<boolean>(false);
+  const [query, setQuery] = React.useState<string>("");
 
-  const products = useSWR(`/product/search?title=${query}`);
+  React.useEffect(
+    () => (query.startsWith("#") ? setSearchByRef(true) : setSearchByRef(false)),
+    [query]
+  );
+
+  const { data: products, isValidating } = useSWR(
+    query.length > 0
+      ? `/product/search?${searchByRef ? "reference" : "title"}=${
+          searchByRef ? query.substring(1) : query
+        }`
+      : null
+  );
 
   return (
     <div className="p-4 BlockProduct">
-      <div className="grid grid-cols-4 gap-2">
-        <div className="col-span-1">
+      {data.productList.map((productId: any, index) => {
+        return (
+          <Product
+            key={index}
+            productIndex={index}
+            productId={productId}
+            data={data}
+            onUpdate={onUpdate}
+          />
+        );
+      })}
+
+      <div className="bg-white border-l-8 border-red-600 rounded-md shadow-md px-20 py-14">
+        <span className="text-2xl font-bold">Ajouter un produit</span>
+        <div className="mt-4 w-2/3">
+          <label htmlFor="product-field">Rechercher</label>
           <input
+            name="product-field"
             type="text"
             value={query}
-            className="w-full px-8 py-4 font-sans text-2xl"
-            placeholder="Find a product inside the catalog"
+            className={`w-full rounded-md placeholder-gray-400 ${
+              searchByRef ? "text-blue-500" : ""
+            }`}
+            placeholder="Référence, nom, ..."
             onChange={(e) => {
               setQuery(e.target.value);
             }}
           />
-          <ul className="font-sans text-2xl border border-gray-400 divide-y divide-gray-400 top-full">
-            {products?.data?.map((product: any) => (
-              <li
-                key={product.id}
-                onClick={() =>
-                  onUpdate({ ...data, productList: [...data.productList, product.id] })
-                }
-                className="px-8 py-4 cursor-pointer hover:bg-gray-100"
-              >
-                {product.i18n.title}
+
+          <ul className="top-full bg-white rounded-md shadow-xl overflow-hidden">
+            {isValidating ? (
+              <li className="text-center py-4 text-2xl text-red-500">
+                <FontAwesomeIcon icon={faCircleNotch} spin />
               </li>
-            ))}
+            ) : (
+              <>
+                {products?.length > 0 ? (
+                  <>
+                    {products
+                      ?.filter((product: any) => !data.productList.includes(product.id))
+                      .map((product: any) => (
+                        <li
+                          key={product.id}
+                          onClick={() => {
+                            onUpdate({
+                              ...data,
+                              productList: [...data.productList, product.id],
+                            });
+                            setQuery("");
+                          }}
+                          className="px-8 py-4 cursor-pointer hover:bg-gray-300 flex flex-col"
+                        >
+                          <span>{product.i18n.title}</span>
+                          <span className="text-gray-400 text-sm">
+                            # {product.reference}
+                          </span>
+                        </li>
+                      ))}
+                  </>
+                ) : query.length > 1 ? (
+                  <li className="px-8 py-4 text-center">
+                    <span>
+                      Aucun résultat{" "}
+                      {query.length > 0 ? (
+                        <span>
+                          pour "
+                          <span
+                            className={`font-bold ${
+                              searchByRef ? "text-blue-500" : "text-gray-500"
+                            }`}
+                          >
+                            {query}
+                          </span>
+                          "
+                        </span>
+                      ) : (
+                        ""
+                      )}
+                    </span>
+                  </li>
+                ) : null}
+              </>
+            )}
           </ul>
         </div>
       </div>
